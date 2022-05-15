efb65f4c8203e9340c6a4ec218c90eac98f8027f
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-05-15 15:07:08 -0300

    terminando con las clases

--- Terrestres.php
+++ Terrestres.php
@@ -78,4 +78,25 @@ class Terrestres extends Viaje{
         }
         return $importe;
     }
+
+
+    // FUNCION PARA CREAR UNA NUEVA VENTA DE PASAJE
+
+    /*La empresa ahora necesita implementar la venta de un pasaje,para ello debe realizar la función venderPasaje(pasajero) que 
+    registra la venta de un viaje al pasajero que es recibido por parámetro. La venta se realiza solo si hayPasajesDisponible. 
+    Si el viaje es terrestre y el asiento es cama, se incrementa el importe un 25%. 
+    Si el viaje es aéreo y el asiento es primera clase sin escalas, se incrementa un 40%, 
+    si el viaje además de ser un asiento de primera clase, el vuelo tiene escalas se incrementa el importe del viaje un 60%. 
+    Tanto para viajes terrestres o aéreos, si el viaje es ida y vuelta, se incrementa el importe del viaje un 50%.
+    El método retorna el importe del pasaje si se pudo realizar la venta. */
+    
+    public function venderPasaje($pasajero){
+        $hay = parent::hayPasajesDisponible();
+        if($hay){
+            $importe = $this->calcularImporte();
+            $this->setImporteViaje($importe);
+            parent::agregarPasajero($pasajero);
+        }
+        return $importe;
+    }
 }
