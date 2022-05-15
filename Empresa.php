ae7120d2602d4ddc228526bfa9177000e3d67f77
Khaleesi89 <93682145+Khaleesi89@users.noreply.github.com>
2022-05-15 16:32:08 -0300

    implementando funcion para agregar a los array de cada viaje distinnto segun su clase

--- Empresa.php
+++ Empresa.php
@@ -37,7 +37,7 @@ class Empresa{
         for ($i=0; $i < count($viajecitos) ; $i++) { 
             $stringViajesAereos.=$viajecitos[$i];
         }
-        return $stringViajes;
+        return $stringViajesAereos;
     }
         
     private function mostrarViajesTerrestres(){
@@ -47,7 +47,7 @@ class Empresa{
         for ($i=0; $i < count($viajecitos) ; $i++) { 
             $stringViajesTerrestres.=$viajecitos[$i];
         }
-        return $stringViajes;
+        return $stringViajesTerrestres;
     }
         
 
@@ -67,37 +67,24 @@ class Empresa{
         return $info;
     }
 
-    // FUNCION PARA CREAR UNA NUEVA VENTA DE PASAJE
 
-    /*La empresa ahora necesita implementar la venta de un pasaje,para ello debe realizar la función venderPasaje(pasajero) que 
-    registra la venta de un viaje al pasajero que es recibido por parámetro. La venta se realiza solo si hayPasajesDisponible. 
-    Si el viaje es terrestre y el asiento es cama, se incrementa el importe un 25%. 
-    Si el viaje es aéreo y el asiento es primera clase sin escalas, se incrementa un 40%, 
-    si el viaje además de ser un asiento de primera clase, el vuelo tiene escalas se incrementa el importe del viaje un 60%. 
-    Tanto para viajes terrestres o aéreos, si el viaje es ida y vuelta, se incrementa el importe del viaje un 50%.
-    El método retorna el importe del pasaje si se pudo realizar la venta. */
+    //AGREGAR VIAJE AL ARRAY DE VIAJES SEGUN SU TIPO //
 
+    public function agregarViajeAlArrayViajes($objViajeSegunTipo){
+        $clase = get_class($objViajeSegunTipo);
+        if ($clase == "Aereos"){
+            $totalAereos = $this->getObjViajesAereos();
+            array_push($totalAereos,$objViajeSegunTipo);
+            $this->setObjViajesAereos($totalAereos);
+        }elseif($clase == "Terrestres"){
+            $totalTerrestres = $this->getObjViajesTerrestres();
+            array_push($totalTerrestres,$objViajeSegunTipo);
+            $this->setObjViajesAereos($totalTerrestres);
 
-    public function venderPasaje($pasajero){
-        $viajes = $this->getObjViajes();
-        $i = 0;
-        $salir = true;
-        while($i< count($viajes) && $salir){
-            $viajeParticular = $viajes[$i];
-            $hay = $viajeParticular->hayPasajesDisponible();
-            if($hay){
-
-            }
-            
-            
-            
-            $i++
-        } 
+        }
     }
+}  
 
-
-    
-}
 
 
 
